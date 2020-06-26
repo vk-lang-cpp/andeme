ead920e09971982fd5d956ea3fb3d412286c5439
Nikolay Belov <nocomentsspb@gmail.com>
2020-06-26 22:19:01 +0300

    Сделал прототип Gui для отправки сообщений

---
+++ qtgui/qtgui.m.cpp
@@ -0,0 +1,35 @@
+#include <QApplication>
+#include <QPushButton>
+#include <ui_qtgui.h>
+#include <iostream>
+#include <QObject>
+#include <messagemanager.h>
+
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    QMainWindow widget;
+    Ui::MainWindow ui;
+    ui.setupUi(&widget);
+    widget.show();
+
+    //MessageManager * m = new MessageManager();
+    MessageManager m(
+        "localhost:12345",
+        [&ui]() -> std::string { 
+            std::string msg = ui.inputMessage->toPlainText().toStdString();
+            ui.inputMessage->clear();
+            return msg;
+        },
+        [&ui](const std::string& msg) -> void {
+            ui.outputMessages->append(msg.c_str());
+        }
+    );
+
+    //QObject::connect(ui.sendMessage, SIGNAL(released()), &m, SLOT(MessageManager::handleButton()));
+    QObject::connect(
+        ui.sendMessage, SIGNAL(released()),
+        &m, SLOT(send()));
+
+    return app.exec();
+}
