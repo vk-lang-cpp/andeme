ce15d682626daf3cba14fbdfa8b72c86232da89e
Nikolay Belov <nocomentsspb@gmail.com>
2020-07-01 16:49:56 +0300

    Fix build system for cross platform

--- thirdparty/build_qt5.cmd
+++ thirdparty/build_qt5.cmd
@@ -8,13 +8,12 @@ git clone -b %VERSION% https://code.qt.io/qt/qt5.git "%WORK_DIR%\qt5"
 
 cd "%WORK_DIR%\qt5"
 
-C:\Strawberry\perl\bin\perl.exe init-repository
+perl.exe init-repository
 
 mkdir "%WORK_DIR%\build"
 cd "%WORK_DIR%\build"
 
-"%WORK_DIR%\qt5\configure" -release  -opensource -nomake examples -nomake tests -confirm-license -skip qtdatavis3d -skip qtdeclarative -skip qtdocgallery -skip qtfeedback -skip qtgamepad -skip qtlocation -skip qtmacextras -skip qtnetworkauth -skip qtpim -skip qtpurchasing -skip qtqa -skip qtquickcontrols -skip qtquickcontrols2 -skip qtremoteobjects -skip qtrepotools -skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip qttranslations -skip qtwebengine -skip qtwebsockets -prefix "%THIRDPARTY_DIR%\qt5" 
-
+"%WORK_DIR%\qt5\configure" -release  -opensource -nomake examples -nomake tests -confirm-license -skip qtdatavis3d -skip qtdeclarative -skip qtdocgallery -skip qtfeedback -skip qtgamepad -skip qtlocation -skip qtmacextras -skip qtnetworkauth -skip qtpim -skip qtpurchasing -skip qtqa -skip qtquickcontrols -skip qtquickcontrols2 -skip qtremoteobjects -skip qtrepotools -skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip qttranslations -skip qtwebengine -skip qtwebsockets -prefix "%THIRDPARTY_DIR%\qt5" && exit 0
 
 nmake
 
