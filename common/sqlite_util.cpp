1f1bfee1fa1594508c23510355b1d2b7983b3a76
Siinthd <Siinthd@protonmail.com>
2020-07-18 01:30:40 +0300

    add SQLite3 path in cmakelists

---
+++ common/sqlite_util.cpp
@@ -0,0 +1,7 @@
+#include "sqlite_util.h"
+#include <iostream>
+#include <sqlite3.h>
+namespace andeme {
+SQLite_util::SQLite_util(){}
+
+}
---
+++ common/sqlite_util.h
@@ -0,0 +1,11 @@
+#ifndef SQLITE_UTIL_H
+#define SQLITE_UTIL_H
+
+namespace andeme {
+class SQLite_util
+{
+public:
+    SQLite_util();
+};
+}
+#endif // SQLITE_UTIL_H
