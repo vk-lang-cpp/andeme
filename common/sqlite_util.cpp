af9e1b2c5627a7afc1daa1b629baf9358b39a3bf
Siinthd <Siinthd@protonmail.com>
2020-08-01 13:06:14 +0300

    callback issue remains

--- common/sqlite_util.cpp
+++ common/sqlite_util.cpp
@@ -1,69 +1,68 @@
-#pragma once
+/*#pragma once
 #include "sqlite_util.h"
 
 
 namespace andeme {
 
 
-    SQLite3Storage::SQLite3Storage()
+    SQLite3Storage::SQLite3Storage(const std::string &filename)
     {
-        std::cout << "constructor\n";
-        sqlite3_initialize();
-
-    }
-
-    SQLite3Storage::~SQLite3Storage()
-    {
-        std::cout << "destructor\n";
-        sqlite3_shutdown();
-        sqlite3_close(m_db);
-    }
-
-
-    bool SQLite3Storage::execute(const std::string& query/*,const Callback& callback*/)
-    {
-        if (sqlite3_exec(m_db, query.c_str(), nullptr, 0, nullptr) != SQLITE_OK)
-            return false;
-        return true;
-    }
-
-    void SQLite3Storage::SetBaseName(const std::string& filename)
-    {
-        m_name=filename;
+        static SqliteGuard guard;
         sqlite3_open_v2(filename.data(), &m_db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, nullptr);
         execute("CREATE table IF NOT EXISTS MESSAGES ("  \
             "ID INT     NOT NULL," \
             "Date		TEXT	NOT NULL," \
-            "MESSAGE	TEXT    NOT NULL,UNIQUE(Date,MESSAGE));"/*,nullptr*/);
+            "MESSAGE	TEXT    NOT NULL,UNIQUE(Date,MESSAGE));",nullptr);
+    }
+
+    bool SQLite3Storage::execute(const std::string& query,const Callback& callback)
+    {
+        return (SQLITE_OK == sqlite3_exec(m_db, query.c_str(), sqlite_callback, (void *)(&callback), nullptr));
     }
 
 
-    bool MessageStorage::add(/*const andeme::schema::Message & msg*/)
+    MessageStorage::MessageStorage(const std::string & dbname):SQLite3Storage(dbname)
+    {
+    }
+
+    bool MessageStorage::add(const andeme::schema::Message & msg)
     {
         //std::string message(msg.text());
         return (this->execute("INSERT INTO MESSAGES ('ID', 'Date', 'MESSAGE') \
-                VALUES ('14',datetime('now','localtime'),'Hello');"/*,nullptr*/));
+                VALUES ('14',datetime('now','localtime'),'Hello');",nullptr));
 
     }
 
-    void MessageStorage::getAll(/*const std::function<bool(const andeme::schema::Message&)>& callback*/)
+    std::vector<andeme::schema::Message> MessageStorage:: getAllMsg(const std::function<bool(const andeme::schema::Message&)>& callback)
     {
-        execute("SELECT * FROM 'MESSAGES';"/*,callback*/);
+        Callback row;
+        execute("SELECT * FROM 'MESSAGES';", row);
+        return std::vector<andeme::schema::Message>();
     }
 
 
-    int callback(void *NotUsed, int argc, char **argv, char **azColName) //подправить под свои нужды
+    int sqlite_callback(void *NotUsed, int argc, char **argv, char **azColName)
     {
+        const andeme::Callback& callback = *static_cast<Callback*>(NotUsed);
 
-        for (int i = 0; i < argc; i++) {
+        // argc - количество столбцов
+        // argv - значения
+        // azColName - имена столбцов
 
-            // Show column name, value, and newline
-            std::cout << argv[i] << " ";
+        std::vector<std::string> result;
+        result.reserve(argc);
+
+        for (size_t i = 0; i < argc; ++i) {
+            result.emplace_back(argv[i]);
         }
-        std::cout << std::endl;
 
-        // Return successful
-        return 0;
+        if (callback(result)) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
 
 }
+*/
--- common/sqlite_util.h
+++ common/sqlite_util.h
@@ -1,28 +1,23 @@
-#ifndef ANDEME_COMMON_SQLITE_UTIL_H
+/*#ifndef ANDEME_COMMON_SQLITE_UTIL_H
 #define ANDEME_COMMON_SQLITE_UTIL_H
 #include <sqlite3.h>
 #include <string>
-#include <memory>
+#include <vector>
 #include <iostream>
-#include "message.pb.h"
+#include <functional>
+#include <schema/message.pb.h>
 
 namespace andeme {
 
-    class SQLite3Storage {
-    public:
-        friend class MessageStorage;
-        static SQLite3Storage& Instance()
-        {
-            static SQLite3Storage mpoSssInstance;
-            return mpoSssInstance;
-        }
-        bool execute(const std::string& query/*, const Callback& callback*/);
-        void SetBaseName(const std::string& filename);
+    using Row = std::vector<std::string>;
+    using Callback = std::function<bool(const std::vector<std::string>&)>;
 
+    class SQLite3Storage {
     protected:
-        static SQLite3Storage* mpoSssInstance;
-        SQLite3Storage();
-        ~SQLite3Storage();
+
+        SQLite3Storage(const std::string&);
+        bool execute(const std::string& query, const Callback& callback);
+
         //copying is prohibited
         SQLite3Storage(SQLite3Storage const &) = delete;
         SQLite3Storage& operator=(SQLite3Storage const &) = delete;
@@ -32,17 +27,30 @@ namespace andeme {
        std::string m_name;
     };
 
-
     class MessageStorage : public SQLite3Storage
     {
     public:
-         bool add(/*const andeme::schema::Message& msg*/); // возвращает true если это новое сообщение, false если уже есть
-         void getAll(/*const std::function<bool(const andeme::schema::Message&)>& callback*/); // передаёт в callback по одному все сообщения
+         MessageStorage(const std::string& dbname);
+         bool add(const andeme::schema::Message& msg); // возвращает true если это новое сообщение, false если уже есть
+         std::vector<andeme::schema::Message> getAllMsg(const std::function<bool(const andeme::schema::Message&)>& callback); // передаёт в callback по одному все сообщения
     private:
     };
 
+        class SqliteGuard {
+    public:
+        SqliteGuard() {
+            sqlite3_initialize();
+        }
 
+        ~SqliteGuard() {
+            std::cout << "shutdown" << std::endl;
+            sqlite3_shutdown();
+        }
+    };
+
+    int sqlite_callback(void *NotUsed, int argc, char **argv, char **azColName);
 
 }
 
 #endif // ANDEME_COMMON_SQLITE_UTIL_H
+*/
